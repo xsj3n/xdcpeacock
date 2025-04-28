8333119c75d6e65b754ff0386820eb653220d11b
xsj3n <xsj3n@tutanota.com>
2025-04-28 11:21:31 -0700

    looks less like ass

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import type { Metadata } from "next";
 import { Anonymous_Pro, Inter } from "next/font/google";
-import "./globals.css";
+import { Honk } from "next/font/google"; 
+import Link from "next/link";
+import Image from "next/image";
+import "./global.css"
 
 const anon_pro = Anonymous_Pro({
   weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap"
+})
+
+const honk = Honk({
+  weight: ["400"],
   subsets: ["latin"],
   display: "swap"
 })
@@ -20,14 +29,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={anon_pro.className}>
+      <body className={`bg-grey-800  ${anon_pro.className}`}>
       <nav className="">
-      <div className="flex justify-between">
-      <p className="p-2">Icon</p>
-      <div className="flex justify-end-safe h-10 gap-4 p-2">
-          <p style={{fontWeight: 700}}>EN</p>
-          <p style={{fontWeight: 700}}>Dark mode button</p>
-          <p style={{fontWeight: 700}}>Buy button</p>
+      <div id="nav" className="flex justify-between border-b border-solid border-black z-10 h-[8vh] sticky">
+        <h2 className={`text-3xl p-2 ${honk.className} mt-2`}>XDCP</h2>
+        <div className="flex justify-end-safe gap-4 p-2">
+          <h2 className={`text-4xl ${honk.className} mt-2`}>EN</h2>
+          <Image src="/light_toggle_trimmed2.png" width={90} height={90} alt="Dark/Light switch"></Image>
+          <h2 className={`text-4xl ${honk.className} mt-2`}>Buy</h2>
         </div>
       </div>
       </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,37 @@
 import Image from "next/image";
+import { Honk } from "next/font/google"; 
+
+
+
+const honk = Honk({
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap"
+})
+
 
 export default function Home() {
   return (
     <main>
-    <div className="grid grid-cols-2 gap-4 p-4">
-      <div className="rounded-md border-2 border-black"></div>
-      <div className="rounded-md border-2 border-black"></div>
-      <div className="rounded-md border-2 border-black"></div>
-      <div className="rounded-md border-2 border-black"></div>
-    </div>
-    <div className="justify-center p-5">
-      <p className="text-center">Icon here</p>
-      <p className="text-center">A memecoin</p>
-    </div>
-    <div className="justify-center gap-1 p-4">
-      <h2 className="p-4 text-center">About XDCPeacock</h2>
-      <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et volutpat justo, ut lacinia ligula. Donec ultrices commodo malesuada. Curabitur placerat lorem urna, nec commodo neque aliquet in. Integer in ipsum ut lorem porta vestibulum. Ut id ligula gravida dui lobortis placerat vitae nec tortor. Fusce elementum non massa ut mattis. Sed laoreet orci ante, sit amet ornare sem cursus quis. </p>
-    </div>
+    <section className="w-fulli min-h-screen sticky bg-[url('../public/bg-colors.jpg')] bg-fixed bg-cover border-b border-solid border-black">
+      <div className="backdrop-blur-sm min-h-screen"></div>
+    </section>
+    <section className="w-full min-h-screen">
+      <div className="grid grid-cols-2 gap-4 p-4">
+        <div className="rounded-md border-2 border-black"></div>
+        <div className="rounded-md border-2 border-black"></div>
+        <div className="rounded-md border-2 border-black"></div>
+        <div className="rounded-md border-2 border-black"></div>
+      </div>
+      <div className="justify-center p-5">
+        <p className="text-center">Icon here</p>
+        <p className={`text-center text-4xl ${honk.className}`}>A memecoin for all, or some shit</p>
+      </div>
+      <div className="justify-center gap-1 p-4">
+        <h2 className={`p-4 text-center text-4xl ${honk.className}`}>About XDCPeacock</h2>
+        <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et volutpat justo, ut lacinia ligula. Donec ultrices commodo malesuada. Curabitur placerat lorem urna, nec commodo neque aliquet in. Integer in ipsum ut lorem porta vestibulum. Ut id ligula gravida dui lobortis placerat vitae nec tortor. Fusce elementum non massa ut mattis. Sed laoreet orci ante, sit amet ornare sem cursus quis. </p>
+      </div>
+    </section>
     </main>
   );
 }
