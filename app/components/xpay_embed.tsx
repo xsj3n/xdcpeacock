40e12d54662d65d2561a7898dae29e7f3a04c123
xsj3n <xsj3n@tutanota.com>
2025-07-18 00:15:30 -0700

    margin change

--- app/components/xpay_embed.tsx
+++ app/components/xpay_embed.tsx
@@ -7,7 +7,7 @@ interface xPayIframeProp {
 export default function XPayIframe({className}: xPayIframeProp) {
   let url: string = "https://app.xspswap.finance/swap#/swap?chainName=xinfin&outputCurrency=0x3b8063BB8F27fEb4eA6224faBfcd281D095fB648&inputCurrency=XDC"
   return(
-  <div className={`${className} flex justify-center`}>
+  <div className={`${className} flex justify-center items-center mt-25`}>
     <div className="relative sm:h-[558px] sm:w-[403px] h-[536px] w-[370px] border-2 border-solid border-black rounded-md" >
       <iframe src={url} className="absolute z-10 sm:h-[554px] sm:w-[396px] h-[532px] w-[366px] rounded-md"  scrolling="no"></iframe>
     </div>
